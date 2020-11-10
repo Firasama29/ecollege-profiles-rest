92fa1f74bf9aec6cc075a44725188d8074188e79
Firasama29 <67781796+Firasama29@users.noreply.github.com>
2020-11-10 17:40:47 +0800

    Add files via upload

---
+++ 4 rest-profiles/src/main/java/com/ecollege/RestProfileApplication.java
@@ -0,0 +1,13 @@
+package com.ecollege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestProfileApplication.class, args);
+	}
+
+}
---
+++ 4 rest-profiles/src/main/java/com/ecollege/rest/ProfileRestController.java
@@ -0,0 +1,67 @@
+package com.ecollege.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecollege.model.Profile;
+import com.ecollege.repository.ProfileRepository;
+import java.util.Optional;
+
+@RestController
+public class ProfileRestController {
+
+	@Autowired
+	ProfileRepository repo;
+	
+	//get all, sorted
+	@GetMapping("profiles")
+	public List<Profile> getAllProfiles(){
+		return repo.findAll();
+	}
+	
+	//get all, ordered by nationality
+	@GetMapping("/ordered")
+	public List<Profile> getProfilesOrderedByNationality(){
+		return repo.getProfilesByOrderByNationality();
+	}
+	
+	//get a profile by id
+	@GetMapping("/profile/{id}")
+	public Optional<Profile> getProfileById(@PathVariable("id") long id){
+		return repo.findById(id);
+	}
+	
+	//register
+	@PostMapping("/add")
+	public Profile register(@RequestBody Profile profile) {
+		return repo.save(profile);
+	}
+	
+	//update
+	@PutMapping("/update/{id}")
+	public Profile update(@PathVariable("id") long id, @RequestBody Profile profile) {
+		repo.findById(id);
+		return repo.save(profile);
+	}
+	
+	//patch
+	@PatchMapping("/patch/{id}")
+	public Profile patch(@PathVariable("id") long id, String firstName, String surName) {
+		return repo.saveDetails(firstName, surName);
+	}
+	
+	//delete
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable("id") long id) {
+		repo.deleteById(id);
+	}
+}
